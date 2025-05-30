b6c3f2b49c14621d42369e28a9d89332af3e03da
MeKebabMan <mekebabman@gmail.com>
2025-05-30 17:31:21 +0200

    Recreated AST structure..

--- include/kebablang/parser.h
+++ include/kebablang/parser.h
@@ -16,94 +16,100 @@ extern "c"
 // Enums
 
 typedef enum {
-    NODE_NUMBER, // 123345123
-    NODE_BOOLEAN, // true / false
-    NODE_STRING, // "Hello, world!"
-    NODE_VARIABLE, // Example: print( VARIABLE )
-    NODE_ASSIGN, // Example: MyVariable = "Hi"
-    NODE_BINARY_OPERATION, // Example: 3 + 4, 5 * 3, etc
-    NODE_FUNCTION_CALL, // Example: print("hello, world")
-    NODE_FUNCTION_DEFINE, // Example: MyFunction(MyArgs) {}
-    NODE_WHILE_LOOP, // Example: while true {}
+    ASTNODE_NUMBER, // A number (Data type is intmax_t) (intmax_t)
+    ASTNODE_STRING, // A string.. (char*)
+    ASTNODE_VARIABLE, // A Variable name.. used for stuff like this print( <MyVariable> ) (char*)
+    ASTNODE_BOOLEAN, // A boolean (true / false)
+    ASTNODE_MATH_OPERATION, // Used for math.. <LEFT> <OPERATOR> <RIGHT> (3 * 2, etc) (struct MathOperation)
+    ASTNODE_VARIABLE_DEFINE, // Used for creating variables.. MyVariable = "Hello, World!" (Struct VariableDefine)
+    ASTNODE_FUNCTION_CALL, // Used for calling functions.. <Function>() (struct FunctionCall)
+    ASTNODE_FUNCTION_DEFINE, // Used for creating functions.. <Function>() { <Body> } 
+    ASTNODE_WHILE_LOOP, // Used for creating a loop.. while <condition> { <Body> }
 } NodeTypes;
 
 typedef enum {
-    NODE_ADD,
-    NODE_MINUS,
-    NODE_TIMES,
-    NODE_DIVIDE,
-    NODE_MODULO,
-} NodeOperator;
+    OP_ADD,
+    OP_SUB,
+    OP_TIMES,
+    OP_POWER,
+    OP_DIVIDE,
+    OP_MODULO
+} MathOperators;
 
 // Structures
 
-typedef struct AssignNode { // Assign structure for assigning variables. Example: <Name> = <Value>
-    char* Name; // Heap allocation 
-    struct ASTNode* Value; // Heap allocation
-} AssignNode;
+typedef struct AST_Args_Arena {
+    struct ASTNode* Args;
+    size_t  Memory;
+    size_t AllocatedMemory;
+} AST_Args;
 
-typedef struct WhileLoopNode { // While Loop structure. Example: while <EXPRESSION> { <BODY> }
-    struct ASTNode* Expression; // Heap allocation (The while loop's condition)
-    struct Body* WhileLoop; // Heap allocation (While loop body)
-} WhileLoopNode;
+typedef struct AST_Body_Arena {
+        struct ASTNode** statements;
+        size_t Memory;
+        size_t AllocatedMemoy;
+} AST_Body_Arena;
 
-typedef struct FunctionCallNode { // A structure for calling functions. Example: <Name>( <Args> )
-    char* Name; // Heap allocation
-    struct FunctionArgs* Args; // Heap allocation (Function Arguments)
-} FunctionCallNode;
+// Structures (AST)
 
-typedef struct FunctionDefineNode { // A structure for creating functions. Example: <Name>( <Args> ) { <FunctionBody> }
-    char* Name; // Heap allocation
-    struct FunctionArgs* Args; // Heap allocation (Function Arguments)
-    struct Body* FunctionBody; // Heap allocation (Function Body)
-} FunctionDefineNode;
+typedef struct AST_WhileLoop {
+    struct ASTNode* Condition;
+    struct AST_Body_Arena* Body;
+} AST_WhileLoop;
 
-typedef struct OperatorNode { // A structure for Binary Operations. Example: <Left> <Type> <Right> ( 1 + 3, 5 * 3, etc )
-    NodeOperator Type; // Operator type
+typedef struct AST_FunctionDefine {
+    char* FunctionName;
+    struct AST_Args_Arena* Args;
+    struct AST_Body_Arena* Body;
+} AST_FunctionDefine;
+
+typedef struct AST_FunctionCall {
+    char* FunctionName;
+    struct AST_Args_Arena* Args;
+} AST_FunctionCall;
+
+typedef struct AST_VariableDefine {
+    char* VariableName;
+    struct ASTNode* VariableValue;
+} AST_VariableDefine;
+
+typedef struct AST_MathOperation {
+    MathOperators Operator;
     struct ASTNode* Left;
-    struct ASTNode* Right;
-} BinaryOperationNode;
+    struct AStNode* Right;
+} AST_MathOperation;
 
-typedef struct FunctionArgs { // Structure for handling Function Arguments. Example: ( <Args> )
-    struct ASTNode* Args; // Heap allocation (Array of arguments)
-    size_t Size;
-} FunctionArgs;
 
-typedef struct Body { // Structure for handling Body. Example: { <Statements> }
-    struct ASTNode* Statements; // Heap allocation (Array of statements)
-    size_t BodySize;
-} BodyNode;
-
-typedef struct ASTNode { // A structure for handling abstract syntax tree nodes (MAIN)
-    NodeTypes Type; // The Type of the node (NODE_NUMBER, NODE_STRING, NODE_VARIABLE_DEFINE, etc)
-    union { // The AST Node's value
-        intmax_t NodeNumber;
-        char* NodeString; // Heap allocation
-        char* NodeVariable; // Heap allocation
-        bool NodeBoolean;
-        BinaryOperationNode NodeBinaryOperation;
-        AssignNode NodeAssign;
-        FunctionDefineNode NodeFunctionDefine;
-        FunctionCallNode NodeFunctionCall;
-        WhileLoopNode NodeWhileLoop;
+typedef struct ASTNode {
+    NodeTypes ASTType;
+    union {
+        intmax_t Number;
+        char* String;
+        char* Variable;
+        bool Boolean;
+        AST_MathOperation Math;
+        AST_VariableDefine VariableDefine;
+        AST_FunctionCall FunctionCall;
+        AST_FunctionDefine FunctionDefine;
+        AST_WhileLoop WhileLoop;
     };
 } ASTNode;
 
-typedef struct AbstractSyntaxTree { // A structure for handling the AST.. kind of like a arena or a pool
-    ASTNode* NodeArray; // Heap allocation
-    size_t NodeMemory;
-    size_t AllocatedNodes;
-} AbstractSyntaxTree;
+typedef struct AST_Arena {
+    ASTNode* Array;
+    size_t Memory;
+    size_t AllocatedMemory;
+} ASTArena;
 
 // Functions
 
 // Uses tokens to ensure correct grammar and syntax for KebabLang and creates a Abstract Syntax Tree for the interpreter
 // Returns AbstractSyntaxTree on success
 // Returns AbstractSyntaxTree with Node* NodeArray as NULL on failure
-AbstractSyntaxTree Parse(TokenArray* Tokens);
+void Parse(TokenArray* Tokens);
 
 // Frees the Abstract Syntax Tree
-void FreeParser(AbstractSyntaxTree* AST);
+void FreeParser(void* AST);
 
 #ifdef __cplusplus
 }
