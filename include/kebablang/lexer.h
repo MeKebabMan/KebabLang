68ac9b4e8fd7cf3944f156a543371c7af3d1bb7f
MeKebabMan <mekebabman@gmail.com>
2025-06-08 17:16:32 +0200

    Added marco

--- include/kebablang/lexer.h
+++ include/kebablang/lexer.h
@@ -31,6 +31,7 @@ typedef enum {
     TOKEN_FALSE, // false (BOOLEAN)
     TOKEN_TRUE, //  true (BOOLEAN)
     TOKEN_COMMA, // ,
+    TOKEN_UNKNOWN, // Useless garbage
 } TokenType;
 
 // structures
@@ -38,6 +39,7 @@ typedef struct Token { // For tokens
     TokenType Token;
     char* Data;
     size_t Length;
+    size_t Line;
 } Token;
 
 typedef struct {
@@ -45,7 +47,7 @@ typedef struct {
     size_t Size; // Allocated Memory for the array
     size_t AllocatedTokens; // How many tokens are allocated
     size_t CursorOffset; // Cursor for the Token array.. (Mainly used by the parser)
-} TokenArray;
+} Lexer;
 
 // Functions
 
@@ -53,10 +55,90 @@ typedef struct {
 // Reads that buffer byte by byte tokenizing and inserting into the TokenArray (Token* Array)
 // Returns TokenArray
 // WARNING: check if TokenArray.Array == NULL because if it is NULL that means it failed to tokenize.. if not then it was successfully tokenized
-TokenArray Tokenize(int fd);
+Lexer Tokenize(int fd);
 
 // Frees every token and array and memory allocated by the lexer
-void FreeLexer(TokenArray* Array);
+void FreeLexer(Lexer* Array);
+
+static inline char* TokenTypeString(TokenType Type) {
+    switch (Type) {
+        case TOKEN_PARENTHESES1:
+            return "TOKEN_PARENTHESES1";
+        break;
+
+        case TOKEN_PARENTHESES2:
+            return "TOKEN_PARENTHESES2";
+        break;
+
+        case TOKEN_SQUARE_BRACKETS1:
+            return "TOKEN_SQAURE_BRACKETS1";
+        break;
+
+        case TOKEN_SQUARE_BRACKETS2:
+            return "TOKEN_SQAURE_BRACKETS2";
+        break;
+
+        case TOKEN_BRACKETS1:
+            return "TOKEN_BRACKETS1";
+        break;
+
+        case TOKEN_BRACKETS2:
+            return "TOKEN_BRACKETS2";
+        break;
+
+        case TOKEN_IDENTIFIER:
+            return "TOKEN_INDENTIFER";
+        break;
+
+        case TOKEN_SINGLE_OPERATOR:
+            return "TOKEN_SINGLE_OPERATOR";
+        break;
+
+        case TOKEN_MULTI_OPERATOR:
+            return "TOKEN_MULTI_OPERATOR";
+        break;
+
+        case TOKEN_STRING:
+            return "TOKEN_STRING";
+        break;
+
+        case TOKEN_NUMBER:
+            return "TOKEN_NUMBER";
+        break;
+
+        case TOKEN_IF:
+            return "TOKEN_IF";
+        break;
+
+        case TOKEN_ELSE:
+            return "TOKEN_ELSE";
+        break;
+
+        case TOKEN_WHILE:
+            return "TOKEN_WHILE";
+        break;
+
+        case TOKEN_TRUE:
+            return "TOKEN_TRUE";
+        break;
+
+        case TOKEN_FALSE:
+            return "TOKEN_FALSE";
+        break;
+
+        case TOKEN_COMMA:
+            return "TOKEN_COMMA";
+        break;
+
+        case TOKEN_FUNCTION:
+            return "TOKEN_FUNCTION";
+        break;
+
+        default:
+            return "TOKEN_UNKNOWN";
+        break;
+    }
+}
 
 #ifdef __cplusplus
 }
