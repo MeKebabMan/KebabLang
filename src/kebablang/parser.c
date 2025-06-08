91504f7d00dfe2961e1d926f8af65e3b3f781e22
MeKebabMan <mekebabman@gmail.com>
2025-06-08 17:17:30 +0200

    Recreating parser

--- src/kebablang/parser.c
+++ src/kebablang/parser.c
@@ -1,29 +1,20 @@
-#include <assert.h>
-#include <inttypes.h>
 #include <kebablang/lexer.h>
 #include <kebablang/parser.h>
-#include <KebabLib.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
 
 // Marcos
-#define AST_FailedParse (Abstract_Syntax_Tree){(void*)NULL, 0, 0}
-#define ASTArgs_FailedParse (AST_Args){(void*)NULL, 0, 0}
-#define ASTBlock_FailedParse (AST_Block){(void*)NULL, 0, 0}
+#define FailedParse (Abstract_Syntax_Tree){(void*)NULL, 0, 0}
+#define Args_FailedParse (AST_Args){(void*)NULL, 0, 0}
+#define Block_FailedParse (AST_Block){(void*)NULL, 0, 0}
+#define Error(Function, Message) fprintf(stderr, "Parser: (%s) %s\n", Function, Message);
 
 // Helpers Declaration (static)
 
 // Parser functions
-static AST_Node* ParseTerm(TokenArray* Tokens);
-static AST_Node* ParseExpression(TokenArray* Tokens);
-static AST_Node* ParseStatement(TokenArray* Tokens);
-static AST_Args ParseArgs(TokenArray* Tokens);
-static AST_Block ParseBlock(TokenArray* Tokens);
+static AST_Node* ParseTerm(Lexer* Tokens);
+static AST_Node* ParseExpression(Lexer* Tokens);
+static AST_Node* ParseStatement(Lexer* Tokens);
+static AST_Args ParseArgs(Lexer* Tokens);
+static AST_Block ParseBlock(Lexer* Tokens);
 
 // Memory handling functions (helpers and static)
 static AST_Node* ReserveNode(AST_Node* NodeArray, size_t* Size, size_t* AllocatedNodes);
@@ -31,17 +22,12 @@ static int Resize_NodeArray(AST_Node** NodeArray, size_t* Size);
 static void FreeASTNode(AST_Node* Node);
 
 // Token handling
-static void NextToken(TokenArray* Tokens);
-static Token* PeekToken(TokenArray* Tokens, size_t Offset);
+static void NextToken(Lexer* Tokens);
+static Token* PeekToken(Lexer* Tokens, size_t Offset);
 
 // Helpers (static)
 
-static AST_Node* ParseTerm(TokenArray* Tokens) {
-
-    return NULL;
-}
-
-static AST_Node* ParseExpression(TokenArray* Tokens) {
+static AST_Node* ParseTerm(Lexer* Tokens) {
 
     if (Tokens == NULL || Tokens->Array == NULL) {
         fprintf(stderr, "Expected NON NULL args.. got null args\n");
@@ -51,7 +37,17 @@ static AST_Node* ParseExpression(TokenArray* Tokens) {
     return NULL;
 }
 
-static AST_Node* ParseStatement(TokenArray* Tokens) {
+static AST_Node* ParseExpression(Lexer* Tokens) {
+
+    if (Tokens == NULL || Tokens->Array == NULL) {
+        fprintf(stderr, "Expected NON NULL args.. got null args\n");
+        return NULL;
+    }
+
+    return NULL;
+}
+
+static AST_Node* ParseStatement(Lexer* Tokens) {
 
     if (Tokens == NULL || Tokens->Array == NULL) {
         fprintf(stderr, "Expected NON NULL args.. got null args\n");
@@ -62,19 +58,27 @@ static AST_Node* ParseStatement(TokenArray* Tokens) {
     return NULL;
 }
 
-static AST_Args ParseArgs(TokenArray* Tokens) {
+static AST_Args ParseArgs(Lexer* Tokens) {
+    if (Tokens == NULL || Tokens->Array == NULL) {
+        fprintf(stderr, "Expected NON NULL args.. got null args\n");
+        return Args_FailedParse;
+    }
 
-    return ASTArgs_FailedParse;
+    return Args_FailedParse;
 }
 
-static AST_Block ParseBlock(TokenArray* Tokens) {
+static AST_Block ParseBlock(Lexer* Tokens) {
+    if (Tokens == NULL || Tokens->Array == NULL) {
+        fprintf(stderr, "Expected NON NULL args.. got null args\n");
+        return Block_FailedParse;
+    }
     
-    return ASTBlock_FailedParse;
+    return Block_FailedParse;
 }
 
 // TOKEN HANDLING FUNCTIONS
 
-static void NextToken(TokenArray* Tokens) {
+static void NextToken(Lexer* Tokens) {
     // Guard Rail
     if (Tokens == NULL || Tokens->Array == NULL || Tokens->CursorOffset >= Tokens->AllocatedTokens) {
         fprintf(stderr, "Expected NON NULL args.. got null args\n");
@@ -84,7 +88,8 @@ static void NextToken(TokenArray* Tokens) {
     Tokens->CursorOffset++; // Next
 }
 
-static Token* PeekToken(TokenArray* Tokens, size_t Offset) {
+static Token* PeekToken(Lexer* Tokens, size_t Offset) {
+    // Guard Rail
     if (Tokens == NULL || Tokens->Array == NULL || (Tokens->CursorOffset + Offset) > Tokens->AllocatedTokens) {
         fprintf(stderr, "Expected NON NULL args.. got null args\n");
         return NULL;
@@ -114,9 +119,13 @@ static void FreeASTNode(AST_Node* Node) {
 
 // Main functions
 
-Abstract_Syntax_Tree Parse(TokenArray *Tokens) {
+Abstract_Syntax_Tree Parse(Lexer* Tokens) {
+    // Guard Rail
+    if (Tokens == NULL || Tokens->Array == NULL) {
+        return FailedParse;
+    }
 
-    return AST_FailedParse;
+    return FailedParse;
 }
 
 void FreeParser(Abstract_Syntax_Tree* AST) {
